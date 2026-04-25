fd70c5440eb8fa21e82b084b205ccaa5dac7fd95
bishnu123-rgb <bishnuparajuli884@gmail.com>
2026-04-25 16:26:31 +0545

    feat: added Parts and Customers controllers

---
+++ AutoBolt.API/Controllers/CustomersController.cs
@@ -0,0 +1,46 @@
+using AutoBolt.Application.DTOs;
+using AutoBolt.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AutoBolt.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class CustomersController(ICustomerService customerService) : ControllerBase
+{
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<CustomerDto>>> GetAll()
+    {
+        var customers = await customerService.GetAllCustomersAsync();
+        return Ok(customers);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<CustomerDto>> GetById(int id)
+    {
+        var customer = await customerService.GetCustomerByIdAsync(id);
+        if (customer == null) return NotFound();
+        return Ok(customer);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<CustomerDto>> Create(CustomerCreateUpdateDto dto)
+    {
+        var createdCustomer = await customerService.CreateCustomerAsync(dto);
+        return CreatedAtAction(nameof(GetById), new { id = createdCustomer.Id }, createdCustomer);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> Update(int id, CustomerCreateUpdateDto dto)
+    {
+        await customerService.UpdateCustomerAsync(id, dto);
+        return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> Delete(int id)
+    {
+        await customerService.DeleteCustomerAsync(id);
+        return NoContent();
+    }
+}
